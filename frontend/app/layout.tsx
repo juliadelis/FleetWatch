fca5a3e0c9cf63e935ad698de3c806f640ce670f
Julia Delis <57239717+juliadelis@users.noreply.github.com>
2026-03-23 21:06:07 +0100

    Initial commit
    feat: project base

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+import "./globals.css";
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning data-grammarly="false">
+      <head>
+        <link rel="icon" href="/icon.ico" />
+      </head>
+      <body suppressHydrationWarning >
+        {children}
+      </body>
+    </html>
+  );
+}
